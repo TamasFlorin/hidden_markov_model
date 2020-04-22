999a282e6f7dd010f6403d4ec55f9a64a464def5
Tamas Florin <tamasflorin@live.com>
2020-04-22 11:58:20 +0300

    Implemented Viterbi and started implementation of backward algorithm.

--- src/hmm.py
+++ src/hmm.py
@@ -1,46 +1,129 @@
 class HiddenMarkovModel(object):
-    """ A probabilistic sequence classifier - given a sequence of units,
-        computes a probability distribution over possible labels and chooses the
-        best label."""
+    """
+    A probabilistic sequence classifier - given a sequence of units,
+    computes a probability distribution over possible labels and chooses the
+    best label.
+    """
 
-    def __init__(self, states, observations, transition_probabilities, emission_probabilities):
-        self.t_p = transition_probabilities
-        self.e_p = emission_probabilities
-        self.st = states
-        self.obs = observations
+    def __init__(self, states, transition_probabilities, emission_probabilities, initial_probabilities):
+        self.transition_probabilities = transition_probabilities
+        self.emission_probabilities = emission_probabilities
+        self.initial_probabilities = initial_probabilities
+        self.states = states
 
-    def forward(self):
-        f = {}
+    def forward(self, observations):
+        """
+        Returns the probability of seeing the given `observations` sequence,
+        using the Forward algorithm.
+        """
+        f = [[0.0 for _ in observations] for _ in self.states]
         forwards = []
 
-        # initialize forward map
-        for i in range(0, len(self.st)):
-            f[self.st[i]] = [0.0 for _ in range(len(self.st))]
-
         # initialization step
-        for i in range(1, len(self.st) - 1):
-            current_state = self.st[i]
-            transition = self.t_p[self.st[0]][current_state]
-            emission = self.e_p[current_state][self.obs[0]]
+        for i, state in enumerate(self.states):
+            transition = self.initial_probabilities[state]
+            emission = self.emission_probabilities[state][observations[0]]
             result = transition * emission
-            f[current_state][0] = result
-            forwards.append((current_state, result))
+            f[i][0] = result
+            forwards.append((state, result))
 
         # recursion step
-        for t in range(1, len(self.obs)):
-            for s in range(1, len(self.st) - 1):
+        for t, observation in enumerate(observations[1:], start=1):
+            for s in range(0, len(self.states)):
                 current_sum = 0
-                for i in range(1, len(self.st) - 1):
-                    f_value = f[self.st[i]][t - 1]
-                    transition = self.t_p[self.st[i]][self.st[s]]
-                    emission = self.e_p[self.st[s]][self.obs[t]]
+                for i in range(0, len(self.states)):
+                    f_value = f[i][t - 1]
+                    transition = self.transition_probabilities[self.states[i]
+                                                               ][self.states[s]]
+                    emission = self.emission_probabilities[self.states[s]
+                                                           ][observation]
                     current_sum += (f_value * transition * emission)
-                f[self.st[s]][t] = current_sum
-                forwards.append((self.st[s], current_sum))
+                f[s][t] = current_sum
+                forwards.append((self.states[s], current_sum))
 
         # termination step
         P = 0.0
-        for s in range(1, len(self.st)):
-            P = P + f[self.st[s]][len(self.obs) - 1]
+        for s in range(0, len(self.states)):
+            P = P + f[s][len(observations) - 1]
 
         return P, forwards
+
+    def backward(self, observations):
+        """
+        Returns the probability of seeing the given `observations` sequence,
+        using the Backward algorithm.
+        """
+
+        b = [[0.0 for _ in observations] for _ in self.states]
+        backwards = []
+        T = len(observations) - 1
+        F = self.states[-1]
+        for i, state in enumerate(self.states):
+            b[i][T] = self.transition_probabilities[state][F]
+            backwards.append((state, b[i][T]))
+
+        # recursion step
+        for t in range(T - 1, 0, -1):
+            next_observation = observations[t + 1]
+            for i, prev_state in enumerate(self.states):
+                current_sum = 0
+                for j, curr_state in enumerate(self.states):
+                    transition = self.transition_probabilities[prev_state][curr_state]
+                    emission = self.emission_probabilities[curr_state][next_observation]
+                    next_value = b[j][t + 1]
+                    current_sum += (transition * emission * next_value)
+                b[i][t] = current_sum
+                backwards.append((prev_state, b[i][t]))
+
+        # termination step
+        P = 0.0
+        first_obs = observations[0]
+        for j, state in enumerate(self.states[1:], start=1):
+            transition = self.initial_probabilities[state]
+            emission = self.emission_probabilities[state][first_obs]
+            P += transition * emission * b[0][j]
+
+        return P, backwards
+
+    def viterbi(self, observations):
+        """
+        Returns the best path of states and the probability that the returned states 
+        will lead to the given observations.
+        """
+        v = [[0.0 for _ in observations] for _ in self.states]
+        backpoints = [[0 for _ in observations] for _ in self.states]
+
+        # Initialization step
+        for s in range(0, len(self.states)):
+            transition = self.initial_probabilities[self.states[s]]
+            emission = self.emission_probabilities[self.states[s]
+                                                   ][observations[0]]
+            result = transition * emission
+            v[s][0] = result
+
+        # recursion step
+        for t in range(1, len(observations)):
+            for s in range(0, len(self.states)):
+                max_v, index_max = 0, 1
+                for i in range(0, len(self.states)):
+                    v_v = v[i][t - 1]
+                    transition = self.transition_probabilities[self.states[i]
+                                                               ][self.states[s]]
+                    emission = self.emission_probabilities[self.states[s]
+                                                           ][observations[t]]
+                    result = v_v * transition * emission
+                    if max_v < result:
+                        max_v = result
+                        index_max = i
+                v[s][t] = max_v
+                backpoints[s][t] = index_max
+
+        # termination step
+        max_v, index_max = 0, 1
+        for i in range(0, len(self.states)):
+            if max_v < v[i][len(observations) - 1]:
+                max_v = v[i][len(observations) - 1]
+                index_max = i
+
+        P = max_v
+        return P, (self.states[index_max], backpoints[index_max])
---
+++ src/hmm_test.py
@@ -0,0 +1,81 @@
+import unittest
+from hmm import HiddenMarkovModel
+
+
+class HmmTest(unittest.TestCase):
+    def setUp(self):
+        self.states = ["rainy", "sunny"]
+
+        self.initial_probabilities = {
+            "rainy": 0.6,
+            "sunny": 0.4,
+        }
+
+        # The probability of moving from state a to state b
+        # such that the sum of the all the probabilities = 1
+        self.transition_probabilities = {
+            "rainy": {
+                "rainy": 0.7,
+                "sunny": 0.3,
+            },
+            "sunny": {
+                "rainy": 0.4,
+                "sunny": 0.6,
+            },
+        }
+
+        # the probability of the observation O being generated from the state q
+        self.emission_probabilities = {
+            "rainy": {
+                "walk": 0.1,
+                "shop": 0.4,
+                "clean": 0.5,
+            },
+            "sunny": {
+                "walk": 0.6,
+                "shop": 0.3,
+                "clean": 0.1,
+            }
+        }
+
+        self.hmm = HiddenMarkovModel(
+            self.states, self.transition_probabilities, self.emission_probabilities, self.initial_probabilities)
+
+    def test_forward(self):
+        observations = ["walk", "shop", "clean"]
+        P, forwards = self.hmm.forward(observations)
+        self.assertEqual(P, 0.033611999999999996)
+        # day 1
+        self.assertEqual(forwards[0], ('rainy', 0.06))
+        self.assertEqual(forwards[1], ('sunny', 0.24))
+
+        # day 2
+        self.assertEqual(forwards[2], ('rainy', 0.0552))
+        self.assertEqual(forwards[3], ('sunny', 0.0486))
+
+        # day 3
+        self.assertEqual(forwards[4], ('rainy', 0.029039999999999996))
+        self.assertEqual(forwards[5], ('sunny', 0.004572))
+
+    def test_backward(self):
+        observations = ["walk", "shop", "clean"]
+        P, backwards = self.hmm.backward(observations)
+        print(backwards)
+        self.assertEqual(P, 0.033611999999999996)
+        # day 1
+        self.assertEqual(backwards[0], ('rainy', 0.06))
+        self.assertEqual(backwards[1], ('sunny', 0.24))
+
+        # day 2
+        self.assertEqual(backwards[2], ('rainy', 0.0552))
+        self.assertEqual(backwards[3], ('sunny', 0.0486))
+
+        # day 3
+        self.assertEqual(backwards[4], ('rainy', 0.029039999999999996))
+        self.assertEqual(backwards[5], ('sunny', 0.004572))
+
+    def test_viterbi(self):
+        observations = ["walk", "shop", "clean"]
+        P, backpoints = self.hmm.viterbi(observations)
+        self.assertEqual(P, 0.01344)
+        self.assertEqual(backpoints, ('rainy', [0, 1, 0]))
